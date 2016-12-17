6f99cdc786bacbc87a740a87bbd7a89aef62e01c
BrowserLMAO <killer.srinks@mail.ru>
2016-12-17 14:36:26 +0300

    Добавил сохранение настроек главного окна

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,26 +4,50 @@
 #include "mainform.h"
 #include "tabbar.h"
 
-namespace Browser {
+const QString fileName = "MainWindow.ini";
 
-MainWindow::MainWindow(QWidget *parent) :
+namespace Browser
+{
+
+  MainWindow::MainWindow(QWidget *parent) :
     QMainWindow {parent},
-    ui {new Ui::MainWindow} {
+    ui {new Ui::MainWindow},
+  settings(fileName , QSettings::IniFormat)
+  {
     ui->setupUi(this);
-
+    //—читать настройки окна
+    LoadSettings();
     connect(ui->pushButton, &QPushButton::clicked,
-            [this] {
-        ui->tabWidget->addTab(new MainForm {}, QString {});
+    [this] {
+      ui->tabWidget->addTab(new MainForm {}, QString {});
     });
-
     connect(ui->tabWidget, &QTabWidget::tabCloseRequested,
-            [this] (int tab) {
-        ui->tabWidget->removeTab(tab);
+    [this](int tab) {
+      ui->tabWidget->removeTab(tab);
     });
-
     emit ui->pushButton->clicked();
-}
+  }
 
-MainWindow::~MainWindow() = default;
+  void MainWindow::SaveSettings()
+  {
+    settings.beginGroup(this->objectName());
+    settings.setValue("geometry", saveGeometry());
+    settings.setValue("state", saveState());
+    settings.endGroup();
+    settings.sync();
+  }
+
+  void MainWindow::LoadSettings()
+  {
+    //      qDebug() << "FILE" << fileName << " DOESN'T EXIST" << endl;
+    if (QFile::exists(fileName)) {
+      settings.beginGroup(this->objectName());
+      restoreGeometry(settings.value("geometry").toByteArray());
+      restoreState(settings.value("state", QByteArray()) .toByteArray());
+      settings.endGroup();
+    }
+  }
+
+  MainWindow::~MainWindow() = default;
 
 } // namespace Browser
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,26 +5,39 @@
 #include <QToolButton>
 #include <QLabel>
 #include <QPushButton>
+#include <QSettings>
+#include <QtGui>
 
 #include <memory>
 
-namespace Browser {
-
-namespace Ui {
-class MainWindow;
-}
-
-class MainWindow final : public QMainWindow
+namespace Browser
 {
-    Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    virtual ~MainWindow();
+  namespace Ui
+  {
+    class MainWindow;
+  }
 
-private:
-    std::unique_ptr<Ui::MainWindow> ui;
-};
+  class MainWindow final : public QMainWindow
+  {
+      Q_OBJECT
+
+    public:
+      explicit MainWindow(QWidget *parent = 0);
+      virtual ~MainWindow();
+
+    private:
+      void closeEvent(QCloseEvent *event)
+      {
+        SaveSettings();
+        event->accept();
+      }
+      void SaveSettings();
+      void LoadSettings();
+    private:
+      std::unique_ptr<Ui::MainWindow> ui;
+      QSettings settings;
+  };
 
 
 } // namespace Browser
